732fdba1552de9e1f241864ee06fc3b28f99aaa3
lratana <samchansreyma@gmail.com>
2025-05-09 16:48:29 +0700

    first projecct

---
+++ src/main/java/com/procurementsystem/procurementsystem/ProcurementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.procurementsystem.procurementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procurementsystem/procurementsystem/ProcurementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procurementsystem.procurementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
